4ca74a5e226899ab8bdca738f02f0780c0b07e50
Franco Capiglioni <fcapiglioni@uoc.edu>
2024-06-07 01:41:44 +0200

    Homepage, section 3 lines

--- js/lineFix.js
+++ js/lineFix.js
@@ -8,6 +8,19 @@ function adjustLine() {
     line2.setAttribute('x2', svgWidth - 61.08);
 
 
+    const svgWidth2 = document.getElementById('section3lines').clientWidth;
+
+    const line3 = document.getElementById('line3');
+    line3.setAttribute('x2', svgWidth2 - 41);
+
+
+    const line5 = document.getElementById('line5');
+    line5.setAttribute('x1', svgWidth2 - 41);
+    line5.setAttribute('x2', svgWidth2 - 41);
+
+
+    const line4 = document.getElementById('line4');
+    line4.setAttribute('x2', svgWidth2 - 41);
 
 
 }
