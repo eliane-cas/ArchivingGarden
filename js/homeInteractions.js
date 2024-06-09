ae3e3217c8c39bf42abb71d525b476f94ec2e7fd
Franco Capiglioni <fcapiglioni@uoc.edu>
2024-06-09 15:09:51 +0200

    Homepage, first interactions

--- js/homeInteractions.js
+++ js/homeInteractions.js
@@ -1,30 +1,29 @@
 function main() {
-    inter1();
-}
+    let indice = 0;
+    const estilos = [
+        { color: "#000000", texto: "And why is this back and forth another word for which is sharing; another phrase for which is mutual aid the gardener's disposition or maybe more to the point, practice? Because a garden a healthy, thriving garden-tells us to. I suspect, whether we know it or not, we're just emulating the garden, which is a repository of sharing.", autor: "Ross Gay,", reference: "Repository of sharing", colorTexto: "white" },
+        { color: "#747D03", texto: "All stories are also the stories of hands — picking up, balancing, pointing, joining, kneading, threading, caressing abandoned in sleep, cutting, eating, wiping, playing music scratching, grasping, peeling, clenching, pulling a trigger, folding.", autor: "John Berger,", reference: "A to X: A Story in Letters", colorTexto: "white" },
+        { color: "#D4FD7D", texto: "Gathering is the tender and thoughtful collection of goods for your kin, and a moment for reunion, for celebration, and for introspection around those goods.", autor: "Mindy Seu,", reference: "Cyberfeminism Index", colorTexto: "black" },
+        { color: "#BAECB7", texto: "Afropresentism is you channeling your ancestry through every technology at your disposal - meditation, conversation, love, the Web - and turning absolutely everything into a portal that takes you precisely where you need to be, in this moment, towards the next. Until finally, the space between the dream and the memory collapses into being your reality - now.", autor: "John Berger,", reference: "A to X: A Story in Letters", colorTexto: "black" },
+        { color: "#7B4B29", texto: "I want to model a collective practice between us: remembering as making sense, and remembering as becoming.", autor: "Sara Hendren,", reference: "Everything you learn you’re only remembering", colorTexto: "white" },
+        { color: "#15D076", texto: "If you have a garden and a library, you have everything you need.", autor: "Marcus Tullius Cicero", reference: "", colorTexto: "black" },
+        { color: "#E6E6E6", texto: "And why is this back and forth another word for which is sharing; another phrase for which is mutual aid the gardener's disposition or maybe more to the point, practice? Because a garden a healthy, thriving garden-tells us to. I suspect, whether we know it or not, we're just emulating the garden, which is a repository of sharing.", autor: "Ross Gay,", reference: "Repository of sharing", colorTexto: "black" },
+        { color: "#000000", texto: "I want to model a collective practice between us: remembering as making sense, and remembering as becoming.", autor: "Sara Hendren,", reference: "Everything you learn you’re only remembering", colorTexto: "white" },
+    ];
 
-function inter1() {
-    function main() {
-        let indice = 0;
-        const estilos = [
-            {
-                color: "#000000", texto: "And why is this back and forth another word for which is sharing; another phrase for which is mutual aid the gardener's disposition or maybe more to the point, practice? Because a garden a healthy, thriving garden-tells us to. I suspect, whether we know it or not, we're just emulating the garden, which is a repository of sharing.", autor: "Ross Gay,", reference: "Repository of sharing", colorTexto: "white"
-            },
-            {
-                color: "#747D03", texto: "All stories are also the stories of hands — picking up, balancing, pointing, joining, kneading, threading, caressing abandoned in sleep, cutting, eating, wiping, playing music scratching, grasping, peeling, clenching, pulling a trigger, folding.", autor: "John Berger,", reference: "A to X: A Story in Letters", colorTexto: "white"
-            },
-            { color: "#D4FD7D", texto: "Gathering is the tender and thoughtful collection of goods for your kin, and a moment for reunion, for celebration, and for introspection around those goods.", autor: "Mindy Seu,", reference: "Cyberfeminism Index", colorTexto: "black" },
-            { color: "#BAECB7", texto: "Afropresentism is you channeling your ancestry through every technology at your disposal - meditation, conversation, love, the Web - and turning absolutely everything into a portal that takes you precisely where you need to be, in this moment, towards the next. Until finally, the space between the dream and the memory collapses into being your reality - now.", autor: "John Berger,", reference: "A to X: A Story in Letters", colorTexto: "black" },
-            { color: "#7B4B29", texto: "I want to model a collective practice between us: remembering as making sense, and remembering as becoming.", autor: "Sara Hendren,", reference: "Everything you learn you’re only remembering", colorTexto: "white" },
-            { color: "#15D076", texto: "If you have a garden and a library, you have everything you need.", autor: "Marcus Tullius Cicero", reference: "", colorTexto: "black" },
-            { color: "#E6E6E6", texto: "And why is this back and forth another word for which is sharing; another phrase for which is mutual aid the gardener's disposition or maybe more to the point, practice? Because a garden a healthy, thriving garden-tells us to. I suspect, whether we know it or not, we're just emulating the garden, which is a repository of sharing.", autor: "Ross Gay,", reference: "Repository of sharing", colorTexto: "black" },
-            { color: "#000000", texto: "I want to model a collective practice between us: remembering as making sense, and remembering as becoming.", autor: "Sara Hendren,", reference: "Everything you learn you’re only remembering", colorTexto: "white" },
-        ];
+    const contenedor = document.getElementById("quotes");
+    const texto = document.getElementById("quote-p");
+    const autor = document.getElementById("quote-author");
+    const reference = document.getElementById("quote-reference");
+    const title = document.querySelector("#quotes .card-title");
+    const subtitle = document.querySelector("#quotes #quoteContainer .sub-card-subtitle");
 
-        const contenedor = document.getElementById("quotes");
-        const texto = document.getElementById("quote-p");
-        const autor = document.getElementById("quote-author");
-        const reference = document.getElementById("quote-reference");
-        const title = document.querySelector("#quotes .card-title");
+    function actualizarEstilos() {
+        if (estilos[indice].colorTexto == "white") {
+            subtitle.style.color = '#FFFFFF80';
+        } else {
+            subtitle.style.color = '#00000080';
+        }
 
         contenedor.style.backgroundColor = estilos[indice].color;
         texto.textContent = estilos[indice].texto;
@@ -34,25 +33,14 @@ function inter1() {
         reference.textContent = estilos[indice].reference;
         reference.style.color = estilos[indice].colorTexto;
         title.style.color = estilos[indice].colorTexto;
-        contenedor.addEventListener('click', function () {
-            contenedor.style.backgroundColor = estilos[indice].color;
-            texto.textContent = estilos[indice].texto;
-            texto.style.color = estilos[indice].colorTexto;
-            autor.textContent = estilos[indice].autor;
-            autor.style.color = estilos[indice].colorTexto;
-            reference.textContent = estilos[indice].reference;
-            reference.style.color = estilos[indice].colorTexto;
-            title.style.color = estilos[indice].colorTexto;
-            indice = (indice + 1) % estilos.length; // Incrementa el índice y vuelve a 0 si alcanza el final
-        });
     }
 
-    if (document.readyState === 'complete') {
-        main();
-    } else {
-        document.addEventListener('DOMContentLoaded', main);
-    }
+    actualizarEstilos();  // Inicializar los estilos y contenidos al cargar
 
+    contenedor.addEventListener('click', function () {
+        indice = (indice + 1) % estilos.length; // Incrementa el índice y vuelve a 0 si alcanza el final
+        actualizarEstilos();  // Actualizar los estilos y contenidos en cada clic
+    });
 }
 
 if (document.readyState === 'complete') {
